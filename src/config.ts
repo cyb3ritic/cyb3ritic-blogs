6259e54d6292e44fdbefb0161ece15b7e06d2f91
cyb3ritic <cyb3ritic@gmail.com>
2024-08-05 18:12:14 +0530

    edit name

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'cyb3ritic',
   bio: 'Penetration testing and Ethical Hacking enthusiast | CTF\'ing at team bi0sblr ',
   links: [
     {
