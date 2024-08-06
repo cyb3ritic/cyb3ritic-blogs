8c25ffb81796d81ab4ea3d57d8bf385511dfeddf
cyb3ritic <cyb3ritic@gmail.com>
2024-08-06 17:06:42 +0530

    edit license card

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
+  site: "https://cyb3riticblogs.pages.dev/",
   base: "/",
   trailingSlash: "always",
   integrations: [
