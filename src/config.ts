47fe59ced3260842f292e564e46a869400dbaada
cyb3ritic <cyb3ritic@gmail.com>
2024-08-24 12:49:23 +0530

    link portfolio

--- src/config.ts
+++ src/config.ts
@@ -39,8 +39,8 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
-      name: 'GitHub',
-      url: 'https://github.com/cyb3ritic',     // Internal links should not include the base path, as it is automatically added
+      name: 'Portfolio',
+      url: 'https://samipshah.com.np',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
     },
   ],
