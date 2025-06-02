b8b15b56af592f2feb654e0f16d7609133d3129f
cyb3ritic <cyb3ritic@gmail.com>
2025-06-03 00:58:16 +0530

    update website address

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,7 +31,7 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://cyb3riticblogs.pages.dev/",
+  site: "https://blog.samipshah.com.np/",
   base: "/",
   trailingSlash: "always",
   integrations: [
